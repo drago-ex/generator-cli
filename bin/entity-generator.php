da9f07d9f3b611093497e1ccfdab7ae143205cd9
accgit <zdenek.papucik@gmail.com>
2020-04-01 13:49:24 +0200

    dev

--- bin/entity-generator.php
+++ bin/entity-generator.php
@@ -5,5 +5,5 @@ require __DIR__ . '/../vendor/autoload.php';
 
 exit(Bootstrap::boot()
 	->createContainer()
-	->getByType(Symfony\Component\Console\Application::class)
+	->getByType(Contributte\Console\Application::class)
 	->run());
