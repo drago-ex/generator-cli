7510b056b4152944cec9e824e5651590f49442cd
Zdeněk Papučík <zdenek.papucik@gmail.com>
2020-05-20 09:57:41 +0200

    Update Bootstrap.php

--- Bootstrap.php
+++ Bootstrap.php
@@ -11,6 +11,7 @@ class Bootstrap
 	public static function boot(): Configurator
 	{
 		$conf = new Configurator;
+		$conf->setDebugMode(true);
 		$conf->enableTracy(__DIR__ . '/log');
 		$conf->setTempDirectory(__DIR__ . '/storage');
 		$conf->addConfig(__DIR__ . '/config.neon');
